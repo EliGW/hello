4ec351de79195d641cf021b6462077de73247704
Eli Wagner <ewagner17@huskers.unl.edu>
2021-08-26 16:19:37 +0000

    initial commit

---
+++ hello.c
@@ -0,0 +1,16 @@
+/**
+ * Author: Eli Wagner
+ * Date: 2021/08/26
+ *
+ * A simple introduction program.
+ */
+ #include <stdlib.h>
+ #include <stdio.h>
+
+ int main(int argc, char **argv) {
+
+     printf("Eli Wagner\n");
+
+     return 0;
+
+ }
